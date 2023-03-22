d486688480f7816684de3e660f622f4df920b039
coliny123 <dh990921@gmail.com>
2023-03-23 01:45:37 +0900

    queue_ex02_ver02

--- queue_ex.py
+++ queue_ex.py
@@ -41,20 +41,25 @@ def peek():
     return queue[(front+1) % SIZE]
 
 
+def cal_time():
+    global SIZE, queue, front, rear
+    total = 0
+    for i in range((front+1) % SIZE, (rear+1) % SIZE):
+        total = total + queue[i][1]
+    return total
+
+
 SIZE = 6
 queue = [None for _ in range(SIZE)]
 front, rear = 0, 0
 
 if __name__ == "__main__":
     call_list = [('고장', 3), ('사용', 9), ('환불', 4), ('기타', 1)]
-    total = 0
-    for i in range(SIZE-1):
-        print(f"귀하의 대기 예상시간은 {total}")
+    for call in call_list:
+        print(f"귀하의 대기 예상시간은 {cal_time()}분 입니다.")
         print(f"현재 대기 콜 : {queue}")
+        enqueue(call)
         print()
-        random.shuffle(call_list)
-        total = total + call_list[0][1]
-        enqueue(call_list[0])
 
     print(f"최종 대기 콜 : {queue}")
     print("프로그램 종료!")
